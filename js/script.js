31496f0fdb76145a86fcac670bf683463d5f8e9e
Higor Oliveira <higorosantos@gmail.com>
2018-12-14 21:42:59 -0200

    novas funçoes adcionada no script

--- js/script.js
+++ js/script.js
@@ -1,7 +1,27 @@
 var saude = 100;
 var fome = 100;
+var dano =  0;
+
+
+tempo();
+
 
 function alimentar (){
     fome++;
     document.getElementById("fome").innerHTML = fome;
+    tempo();
 }
+function tempo() {
+    
+    
+    
+    setInterval(function(){ 
+        fome = fome - dano;
+        dano = Math.floor(Math.random() * 10);;
+      
+        document.getElementById("fome").innerHTML = fome;
+        
+    }, 3000);
+  
+}
+console.log(dano);
