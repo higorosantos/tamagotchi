e26ed3bcc088419bda3138c61534bd4d9e5d49bc
Higor Oliveira <higorosantos@gmail.com>
2018-12-15 21:57:27 -0200

    funcao morte adcionada

--- js/script.js
+++ js/script.js
@@ -5,22 +5,31 @@ var dano =  1;
 
 tempo();
 
+function dead (){
+    fome = fome;
+    if (fome <= 0){
+        confirm("morri meno");
+        location.reload();
+    }
+}
 
 function alimentar (){
         fome = fome + dano;
-        document.getElementById("fome").innerHTML = fome;
+        document.getElementById("fome").innerHTML = fome + "%";
         document.getElementById("fomebar").style.width =  fome + "%";
 
 }
+
 function tempo() {
     
     setInterval(function(){ 
         fome = fome - dano;
-        dano = Math.floor(Math.random() * 15);;
+        dano = Math.floor(Math.random() * 10);;
     
-        document.getElementById("fome").innerHTML = fome;
+        document.getElementById("fome").innerHTML = fome + "%" ;
         document.getElementById("fomebar").style.width =  fome + "%";
-        
+        dead();
+    
         
     }, 2000);
   
